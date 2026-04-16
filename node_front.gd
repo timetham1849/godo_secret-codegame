63ed773d7bec010c8062c27d2d15e52c6c33f9a1
timetham1849 <timetham1849@gmail.com>
2026-04-16 16:07:42 +0700

    invit

--- node_front.gd
+++ node_front.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+var detail_selec1= false
 func _on_collision_shape_2d_item_rect_changed() -> void:
 	pass # Replace with function body.
 
@@ -16,5 +16,12 @@ func _on_ลูกศรขวาf_pressed() -> void:
 	get_tree().change_scene_to_file("res://node_right.tscn")
 
 
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	pass # Replace with function body.
+func _on_nonezoom_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		$"กระดานดำขยาย".visible =true	
+		detail_selec1 = true
+
+func _on_zoom_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		$"กระดานดำขยาย".visible =false
+		detail_selec1 = false
