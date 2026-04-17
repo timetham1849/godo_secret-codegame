1ab70930bbdaabf863c1f647b2a83768ac02a3c6
timetham1849 <timetham1849@gmail.com>
2026-04-17 18:17:21 +0700

    invit

---
+++ node_mainmenu.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://node_front.tscn")
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://Setting.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit() # Replace with function body.
--- start.gd
+++ start.gd
@@ -7,5 +7,3 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	get_tree().change_scene_to_file("res://node_front.tscn")
