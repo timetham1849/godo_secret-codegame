1efca3a7fb5c16c5ddb134c5d250ba2131cfa188
timetham1849 <timetham1849@gmail.com>
2026-04-18 12:59:27 +0700

    invit

--- node_right.gd
+++ node_right.gd
@@ -5,3 +5,8 @@ func _on_ลูกศรซ้ายr_pressed() -> void:
 
 func _on_ลูกศรขวาr_pressed() -> void:
 	get_tree().change_scene_to_file("res://node_black.tscn")
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		get_tree().change_scene_to_file("res://right_to_table.tscn")
